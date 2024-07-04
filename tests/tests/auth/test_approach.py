8ad7e219f0a0f9ddea09c2bef640f3d80c3ac254
JesusBandez <jesusbandezt@gmail.com>
2024-07-04 13:37:16 -0400

    fixed approach tests

--- tests/tests/auth/test_approach.py
+++ tests/tests/auth/test_approach.py
@@ -12,7 +12,8 @@ def create_approach(page: Page, approach: Approach) -> None:
 
     page.get_by_placeholder("Nuevo tipo de abordaje").fill(approach.name)
     page.get_by_text("Guardar").click()
-    page.reload()
+    page.wait_for_timeout(2000)
+    page.reload()    
 
 def test_create_approach(page: Page, admin: User):  # noqa: F811
     "Crear un abordaje"
@@ -20,7 +21,7 @@ def test_create_approach(page: Page, admin: User):  # noqa: F811
     login_with_user(page, admin)
     new_approach = Approach.create_fake()
     create_approach(page, new_approach)
-    page.wait_for_timeout(500)
+    page.wait_for_timeout(2000)
     
 def test_remove_approach(page: Page, admin: User):  # noqa: F811
     "Eliminar un abordaje"
@@ -29,11 +30,11 @@ def test_remove_approach(page: Page, admin: User):  # noqa: F811
     new_approach = Approach.create_fake()
     create_approach(page, new_approach)
     page.get_by_text("Modificar abordajes").click()
-    page.get_by_role("button", name="Eliminar", exact=True).last.click()
-    page.get_by_text("Eliminar", exact=True).click()
+    page.locator("xpath=//div[@class='ReactModal__Content ReactModal__Content--after-open']//button[@title='Eliminar']").first.click()
+    page.wait_for_timeout(2000)
     page.reload()
     page.get_by_text("Modificar abordajes").click()
-    page.wait_for_timeout(500)
+    page.wait_for_timeout(2000)
     
 def test_edit_approach(page: Page, admin: User):  # noqa: F811
     "Edita un abordaje"
@@ -42,10 +43,11 @@ def test_edit_approach(page: Page, admin: User):  # noqa: F811
     new_approach = Approach.create_fake()    
     create_approach(page, new_approach)
     edited_approach = Approach.create_fake()
+    page.wait_for_timeout(2000)
     page.get_by_text("Modificar abordajes").click()
-    page.get_by_role("button", name="Editar", exact=True).first.click()
+    page.locator("xpath=//div[@class='ReactModal__Content ReactModal__Content--after-open']//button[@title='Editar']").first.click()
     page.get_by_placeholder("Editar tipo de abordaje").fill(edited_approach.name)
     page.get_by_role("button", name="Listo").click()
     page.reload()
     page.get_by_text("Modificar abordajes").click()
-    page.wait_for_timeout(500)
+    page.wait_for_timeout(2000)
